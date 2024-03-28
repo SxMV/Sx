e22f50abf0f07394f97b7405ec2f4a405395ec66
Tauã Miguel <98820695+cringgameplays@users.noreply.github.com>
2024-03-28 01:05:10 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -14,7 +14,7 @@ function sendToDiscord() {
     var photo = photoInput.files[0]; // Obter a foto selecionada pelo usuário
 
     // Verificar se todos os campos foram preenchidos
-    if (!name || !email || !message || !photo) {
+    if (!name || !email || !message ) {
         $('#response').text('Por favor, preencha todos os campos e selecione uma foto.').addClass('error').fadeIn();
         setTimeout(function(){
             $('#response').fadeOut();
@@ -22,38 +22,33 @@ function sendToDiscord() {
         return;
     }
 
-    // Codificar a foto em Base64
-    var reader = new FileReader();
-    reader.readAsDataURL(photo);
-    reader.onload = function () {
-        var base64Photo = reader.result.split(',')[1]; // Remover o prefixo 'data:image/png;base64,'
+    var formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('message', message);
+    formData.append('photo', photo);
 
-        // Montar a mensagem a ser enviada para o Discord
-        var discordMessage = 'Nome: ' + name + '\n';
-        discordMessage += 'E-mail: ' + email + '\n\n';
-        discordMessage += 'Mensagem:\n' + message + '\n\n';
-        discordMessage += 'Foto:\n' + base64Photo;
+    // URL do Webhook do Discord
+    var webhookUrl = 'https://discord.com/api/webhooks/1222733165395837009/nQxy-tWh-_0jBx5RIs8bKr_rz6NiOetPfO4VdaZeUIEZ1k5H4UiUziRziJpeChHYfLmc';
 
-        // URL do Webhook do Discord
-        var webhookUrl = 'https://discord.com/api/webhooks/1222733165395837009/nQxy-tWh-_0jBx5RIs8bKr_rz6NiOetPfO4VdaZeUIEZ1k5H4UiUziRziJpeChHYfLmc';
-
-        // Enviar mensagem para o Discord via Webhook
-        $.ajax({
-            type: "POST",
-            url: webhookUrl,
-            data: { content: discordMessage }, // Removido o JSON.stringify
-            success: function(response) {
-                $('#response').text('Mensagem enviada com sucesso para o Discord').addClass('success').fadeIn();
-                setTimeout(function(){
-                    $('#response').fadeOut();
-                }, 5000);
-            },
-            error: function(xhr, status, error) {
-                $('#response').text('Erro ao enviar mensagem para o Discord. Por favor, tente novamente mais tarde.').addClass('error').fadeIn();
-                setTimeout(function(){
-                    $('#response').fadeOut();
-                }, 5000);
-            }
-        });
-    };
+    // Enviar mensagem para o Discord via Webhook
+    $.ajax({
+        type: "POST",
+        url: webhookUrl,
+        data: formData,
+        processData: false,  // Não processar os dados
+        contentType: false,  // Não definir o tipo de conteúdo
+        success: function(response) {
+            $('#response').text('Mensagem enviada com sucesso para o Discord').addClass('success').fadeIn();
+            setTimeout(function(){
+                $('#response').fadeOut();
+            }, 5000);
+        },
+        error: function(xhr, status, error) {
+            $('#response').text('Erro ao enviar mensagem para o Discord. Por favor, tente novamente mais tarde.').addClass('error').fadeIn();
+            setTimeout(function(){
+                $('#response').fadeOut();
+            }, 5000);
+        }
+    });
 }
